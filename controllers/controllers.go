9592b34efe8266a4f8b3c94ab3cd4007f56cb263
albertoparente <albertoparentefh@gmail.com>
2022-02-09 23:23:47 -0300

    Creating deletion and editing

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,33 +4,51 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/go-api-rest/database"
 	"github.com/go-api-rest/models"
+	"github.com/gorilla/mux"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
 }
 
-func GetByAllPersonalities(w, http.ResponseWriter, r *http.Request) {
-	var p []models.Personality
-	database.DB.Find(&p)
-	json.NewEncoder(w).Encode(p)
-}
-
-func GetByPersonality(w, http.ResponseWriter, r *http.Request) {
-	vars := mux.vars(r)
-	id := vars["id"]
-	var personality models.Personalities
-	database.DB.First(&p, id)
+func GetByAllPersonalities(w http.ResponseWriter, r *http.Request) {
+	var personality []models.Personality
+	database.DB.Find(&personality)
 	json.NewEncoder(w).Encode(personality)
 }
 
-func NewPersonality(w, http.ResponseWriter, r *http.Request) {
+func GetByPersonality(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personality models.Personality
+	database.DB.First(&personality, id)
+	json.NewEncoder(w).Encode(personality)
+}
+
+func NewPersonality(w http.ResponseWriter, r *http.Request) {
 	var personality models.Personality
 	json.NewDecoder(r.Body).Decode(personality)
 	database.DB.Create(&personality)
 	json.NewEncoder(w).Encode((personality))
+}
+
+func DeletePersonality(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personality models.Personality
+	database.DB.Delete(&personality, id)
+	json.NewEncoder(w).Encode(personality)
+}
+
+func EditPersonality(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personality models.Personality
+	database.DB.First(&personality, id)
+	json.NewDecoder(r.Body).Decode(&personality)
+	database.DB.Save(&personality)
+	json.NewEncoder(w).Encode(personality)
 }
--- database/db.go
+++ database/db.go
@@ -14,7 +14,7 @@ var (
 
 func ConnectDatabase() {
 	stringToConnection := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(stringToConnection))
+	DB, err = gorm.Open(postgres.Open(stringToConnection))
 	if err != nil {
 		log.Panic("Error connecting to database...!")
 	}
